02b5e0b2f41509770f0515ed58e99912b6117a1f
Justin Javan <6845132+jjjavan@users.noreply.github.com>
2018-06-11 15:39:47 -0700

    Add files via upload

--- Assignment1/PercolationStats.java
+++ Assignment1/PercolationStats.java
@@ -7,8 +7,8 @@ public class PercolationStats
     private final int trials;
     private final double[] run;
     private static final double CONFIDENCE_95 = 1.96;
-    private double mean;
-    private double stdDev;
+    private final double mean;
+    private final double stdDev;
 
     public PercolationStats(int n, int trials)
     {
@@ -26,14 +26,13 @@ public class PercolationStats
                 x = StdRandom.uniform(1,n+1);
                 y = StdRandom.uniform(1,n+1);
                 p.open(x, y);
-            }
+            }//while
 
             run[i] = p.numberOfOpenSites()/Math.pow((double)n,2);
+
+        }//for
             mean = StdStats.mean(run);
             stdDev = StdStats.stddev(run);
-            p = null;
-        }
-
 
     }// perform trials independent experiments on an n-by-n grid
 
@@ -50,15 +49,14 @@ public class PercolationStats
 
     public double confidenceLo()
     {
-        double T = Math.sqrt(trials);
-        return mean - CONFIDENCE_95*stdDev/T;
+
+        return mean - (CONFIDENCE_95*stdDev / Math.sqrt(trials));
 
     }// low endpoint of 95% confidence interval
 
     public double confidenceHi()
     {
-        double T = Math.sqrt(trials);
-        return mean + CONFIDENCE_95*stdDev/T;
+        return mean + (CONFIDENCE_95*stdDev/Math.sqrt(trials));
 
     }// high endpoint of 95% confidence interval
     private void validate(int n, int trials)
@@ -67,13 +65,15 @@ public class PercolationStats
             throw new IllegalArgumentException("Size of set and trials must be greater than 0. You entered " + n
                     + " for size and " + trials + " for trials.\n");
     }// were valid values entered?
+
     public static void main(String[] args)
     {
         int n = Integer.parseInt(args[0]);
-        int trials = Integer.parseInt(args[1]);
+        int trials = Integer.parseInt(args[0]);
         PercolationStats percStats = new PercolationStats(n, trials);
-        System.out.printf("mean                    = %.16f%n",percStats.mean());
-        System.out.printf("stddev                  = %.16f%n",percStats.stddev());
-        System.out.printf("95% confidence interval = [%.16f %.16f]%n", percStats.confidenceLo(), percStats.confidenceHi());
+        System.out.printf("mean                     = %.16f%n",percStats.mean());
+        System.out.printf("stddev                   = %.16f%n",percStats.stddev());
+        System.out.printf("95%% confidence interval = [%.16f %.16f]%n", percStats.confidenceLo(), percStats.confidenceHi());
+
     }
 }
